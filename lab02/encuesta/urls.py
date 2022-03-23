0f979389d71c1a6d897e4b5c8ede849b3cc1435c
Daniel Tapia <dtapias@unsa.edu.pe>
2022-03-22 21:53:41 -0500

    Se agrego las paginas de la tarea

--- lab02/encuesta/urls.py
+++ lab02/encuesta/urls.py
@@ -7,4 +7,8 @@ app_name = 'encuesta'
 urlpatterns = [
     path('', views.index, name='index'),
     path('enviar', views.enviar, name='enviar'),
+    path('operaciones', views.operaciones, name='operaciones'),
+    path('operaciones_respuesta', views.operaciones_respuesta, name='operaciones_respuesta'),
+    path('cilindro', views.cilindro, name='cilindro'),
+    path('cilindro_respuesta', views.cilindro_respuesta, name='cilindro_respuesta'),
 ]
--- lab02/encuesta/views.py
+++ lab02/encuesta/views.py
@@ -2,6 +2,8 @@ from ast import Return
 from importlib.resources import contents
 from multiprocessing import context
 from django.shortcuts import render
+import parser
+import math
 
 # Create your views here.
 def index(request):
@@ -18,7 +20,33 @@ def enviar(request):
         'clave' : request.POST['password'],
         'educacion' : request.POST['educacion'],
         'nacionalidad' : request.POST['nacionalidad'],
-        'idiomas' : request.POST.getlist['idiomas'],
+        'idiomas' : request.POST.getlist('idiomas'),
         'correo' : request.POST['email'],
+        'website' : request.POST['sitioweb'],
     }
     return render(request, 'encuesta/respuesta.html', context)
+
+def operaciones_respuesta(request):
+    exprecion = parser.expr(request.POST['num_1'] + request.POST['operaciones'] + request.POST['num_2']).compile()
+    resultado = eval(exprecion)
+    context = {
+        'titulo' : 'Respuesta',
+        'resultado' : resultado,
+    }
+    return render(request, 'encuesta/operaciones_respuesta.html', context)
+
+def operaciones(request):
+    return render(request, 'encuesta/operaciones.html')
+
+def cilindro_respuesta(request):
+    exprecion = parser.expr("((" + request.POST['num_2'] + "/2)**2)*" + request.POST['num_1']).compile()
+    resultado = float(eval(exprecion)) * math.radians(180)
+    
+    context = {
+        'titulo' : 'Respuesta',
+        'resultado' : resultado,
+    }
+    return render(request, 'encuesta/cilindro_respuesta.html', context)
+
+def cilindro(request):
+    return render(request, 'encuesta/cilindro.html')
