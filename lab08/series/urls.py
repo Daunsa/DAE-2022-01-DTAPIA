6142bdade1a87febdd4a9de7357463bb1fb06d44
Daniel Tapia <dtapias@unsa.edu.pe>
2022-05-03 10:58:36 -0500

    Se termino el laboratorio

---
+++ lab08/series/urls.py
@@ -0,0 +1,10 @@
+from django import views
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('',views.IndexView.as_view(),name='index'),
+    path('series',views.SeriesView.as_view(),name='series'),
+    path('serie/<int:serie_id>',views.SerieDetailView.as_view()),
+]
